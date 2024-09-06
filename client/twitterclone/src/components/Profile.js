c7d84dd0b796078fab1f69c4ffe15ec6dc7dbe4b
Rakesh Das <18.rakeshdas@gmail.com>
2024-09-06 20:23:30 +0530

    Built the profile

--- client/twitterclone/src/components/Profile.js
+++ client/twitterclone/src/components/Profile.js
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from "react";
+import { IoArrowBack } from "react-icons/io5";
 
 function Profile() {
   return (
-    <div>
-      
+    <div className="w-[50%]">
+      <div>
+        <div className="flex items-center py-2">
+          <div className="p-2 rounded-full hover:bg-gray-200 cursor-pointer">
+          <IoArrowBack size="24px"/>
+          </div>
+          <div className="ml-2">
+          <h1 className="font-bold text-lg">Rakesh Das</h1>
+          <p className="text-gray-500 text-sm">2600 Posts</p>
+          </div>
+        </div>
+        <img
+          src="https://pbs.twimg.com/profile_banners/1751470922379489280/1714232500/1080x360"
+          alt="banner"
+        />
+      </div>
     </div>
-  )
+  );
 }
 
-export default Profile
+export default Profile;
