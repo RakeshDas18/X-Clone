ef359c4c0c323dc7881bcf972cde712ae7e4f54f
Rakesh Das <18.rakeshdas@gmail.com>
2024-09-07 21:22:13 +0530

    Fix the button text

--- client/twitterclone/src/components/Login.js
+++ client/twitterclone/src/components/Login.js
@@ -4,7 +4,7 @@ function Login() {
   const [isLogin, setIsLogin] = useState(true);
   const loginSignupHandler = () => {
     setIsLogin(!isLogin);
-  }
+  };
   return (
     <div className="w-screen h-screen flex items-center justify-center">
       <div className="flex items-center justify-evenly w-[80%]">
@@ -15,7 +15,9 @@ function Login() {
           <div className="my-5">
             <h1 className="font-bold text-6xl">Happening Now</h1>
           </div>
-          <h1 className="mt-4 mb-2 text-2xl font-bold">{isLogin ? "Login" : "Register"}</h1>
+          <h1 className="mt-4 mb-2 text-2xl font-bold">
+            {isLogin ? "Login" : "Register"}
+          </h1>
           <form className="flex flex-col w-[70%]">
             {!isLogin && (
               <>
@@ -43,10 +45,16 @@ function Login() {
               className="outline-blue-500 border border-gray-800 px-3 py-1 rounded-full my-1 font-semibold"
             />
             <button className="bg-[#1D9BF0] border-none py-2 my-4 rounded-full text-lg text-white">
-              Login
+              {isLogin ? "Login" : "Create account"}
             </button>
             <h1>
-              {isLogin ? "Don't have an account?" : "Already have an account?"} <span className="font-bold text-blue-600 cursor-pointer" onClick={loginSignupHandler}>{isLogin ? "Register" : "Login"}</span>
+              {isLogin ? "Don't have an account?" : "Already have an account?"}{" "}
+              <span
+                className="font-bold text-blue-600 cursor-pointer"
+                onClick={loginSignupHandler}
+              >
+                {isLogin ? "Register" : "Login"}
+              </span>
             </h1>
           </form>
         </div>
