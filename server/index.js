a411f1144d576c13dd0e71b49f20f91799cf731f
Rakesh Das <18.rakeshdas@gmail.com>
2024-09-08 19:44:34 +0530

    Establish the db connection

--- server/index.js
+++ server/index.js
@@ -1,11 +1,14 @@
 import express from "express";
 import dotenv from "dotenv"
+import databaseConnection from "./config/database.js";
 
-const app = express();
 
 dotenv.config({
     path:".env"
 })
+
+databaseConnection()
+const app = express();
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is lisiting at port ${process.env.PORT}`)
