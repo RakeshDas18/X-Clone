219bc6e9a6456e0add4249d080e29ffa004086f9
Rakesh Das <18.rakeshdas@gmail.com>
2024-09-09 21:15:41 +0530

    Added Middleware and Routes

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import dotenv from "dotenv"
 import databaseConnection from "./config/database.js";
+import cookieParser from "cookie-parser";
+import userRoute from "./routes/userRoute.js"
 
 
 dotenv.config({
@@ -9,6 +11,16 @@ dotenv.config({
 
 databaseConnection();
 const app = express();
+
+//Middlewares
+app.use(express.urlencoded({
+    extends:true
+}))
+app.use(express.json());
+app.use(cookieParser());
+
+//routes
+app.use("/api/v1/user", userRoute)
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is lisiting at port ${process.env.PORT}`)
