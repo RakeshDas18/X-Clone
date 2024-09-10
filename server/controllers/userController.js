1bf72912e65853765aac007962610ad9eb5d7aef
Rakesh Das <18.rakeshdas@gmail.com>
2024-09-10 21:41:47 +0530

    Coded the login controller

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,5 +1,6 @@
 import {User} from "../models/userSchema.js"
 import bcryptjs from "bcryptjs"
+import jwt from "jsonwebtoken"
 
 export const Register = async (req, res) => {
         try {
@@ -35,4 +36,44 @@ export const Register = async (req, res) => {
         } catch (error) {
             console.log(error);            
         }
+}
+
+export const Login = async (req, res) => {
+    try {
+        const {email, password} = req.body;
+        if(!email || !password){
+            return res.status(401).json({
+                message:"All fields are required!",
+                success:false
+            })
+        };
+
+        const user = await findOne({email});
+        if(!user){
+            return res.status(401).json({
+                message:"Incorrect email or password!",
+                success:false
+            })
+        }
+        const isMatch = await bcryptjs.compare(user.password, password);
+
+        if(!isMatch){
+            return res.status(401).json({
+                message:"Incorrect email or password!",
+                success:false
+            })
+        }
+
+        const tokenData = {
+            userId:user_id
+        }
+        const token = await jwt.sign({tokenData}, process.env.TOKEN_SECERT, {expiresIn:"1d"})
+
+        return res.status(201).cookie("token", token, {expiresIn:"1d", httpOnly:true}).json({
+            message:`Welcome back ${user.name}`,
+            success:true
+        })
+    } catch (error) {
+        console.log(error);        
+    }
 }
