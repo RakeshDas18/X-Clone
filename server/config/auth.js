df06f6f6cd4edba739e8cc53f0d662f2e3a415dd
Rakesh Das <18.rakeshdas@gmail.com>
2024-09-11 20:55:02 +0530

    Modified for tweet

--- server/config/auth.js
+++ server/config/auth.js
@@ -5,9 +5,9 @@ dotenv.config({
     path:"../config/.env"
 })
 
-const isAuthenticated = async (req, res, next) => {
+export const isAuthenticated = async (req, res, next) => {
     try {
-        console.log(token);
+        const token = req.cookies.token
         if(!token){
             return res.status(401).json({
                 message:"User not authenticated!",
@@ -16,8 +16,7 @@ const isAuthenticated = async (req, res, next) => {
         }
 
         const decode = await jwt.verify(token, process.env.TOKEN_SECRET);
-        console.log(decode);
-        req.user = decode.id
+        req.user = decode.userId
         next();        
     } catch (error) {
         console.log(error);        
