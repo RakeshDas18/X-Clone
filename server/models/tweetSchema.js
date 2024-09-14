53dc69e3587207db776e421684c01a1e9e639bca
Rakesh Das <18.rakeshdas@gmail.com>
2024-09-14 15:02:02 +0530

    Changed the bookmarks field

--- server/models/tweetSchema.js
+++ server/models/tweetSchema.js
@@ -13,10 +13,6 @@ const tweetSchema = new mongoose.Schema({
         type:mongoose.Schema.Types.ObjectId,
         ref:"User"
     },
-    bookmarks:{
-        type:Array,
-        default:[]
-    },
     
 }, {timestamps:true})
 
