b5b6d66bc9fbb28c1d3bd49c1d8a89b9cd6d438c
Rakesh Das <18.rakeshdas@gmail.com>
2024-09-15 21:45:13 +0530

    Coded the getMyProfile() for accessing others profile

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -109,3 +109,15 @@ export const bookmark = async (req, res) => {
         console.log(error);        
     }
 }
+
+export const getMyProfile = async (req, res) => {
+    try {
+        const id = req.params.id;
+        const user = await User.findById(id);
+        return res.status(200).json({
+            user,
+        })
+    } catch (error) {
+        console.log(error);        
+    }
+}
