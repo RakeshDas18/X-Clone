8ffda48a83aaffcf7b5ae393511c19daf74f0b54
Rakesh Das <18.rakeshdas@gmail.com>
2024-09-16 21:30:11 +0530

    Added the follow route to follow other users

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,6 +1,6 @@
 import express from "express"
 import isAuthenticated from "../config/auth.js";
-import { bookmark, getMyProfile, getOtherUsers, Login, logout, Register } from "../controllers/userController.js";
+import { bookmark, follow, getMyProfile, getOtherUsers, Login, logout, Register } from "../controllers/userController.js";
 
 const router = express.Router();
 
@@ -10,5 +10,6 @@ router.route("/logout").get(logout);
 router.route("/bookmark/:id").put(isAuthenticated, bookmark);
 router.route("/profile/:id").get(isAuthenticated, getMyProfile);
 router.route("/otheruser/:id").get(isAuthenticated, getOtherUsers);
+router.route("/follow/:id").post(isAuthenticated, follow);
 
 export default router
