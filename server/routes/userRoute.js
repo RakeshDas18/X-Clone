41ebda4073fbf5e0efd850e493919eacc7212793
Rakesh Das <18.rakeshdas@gmail.com>
2024-09-16 21:05:00 +0530

    Added the getOtherUser route

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,6 +1,6 @@
 import express from "express"
 import isAuthenticated from "../config/auth.js";
-import { bookmark, getMyProfile, Login, logout, Register } from "../controllers/userController.js";
+import { bookmark, getMyProfile, getOtherUsers, Login, logout, Register } from "../controllers/userController.js";
 
 const router = express.Router();
 
@@ -9,5 +9,6 @@ router.route("/login").post(Login);
 router.route("/logout").get(logout);
 router.route("/bookmark/:id").put(isAuthenticated, bookmark);
 router.route("/profile/:id").get(isAuthenticated, getMyProfile);
+router.route("/otheruser/:id").get(isAuthenticated, getOtherUsers);
 
 export default router
