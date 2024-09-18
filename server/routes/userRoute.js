49674db4cecfc2481347871d8709ca299cd8000c
Rakesh Das <18.rakeshdas@gmail.com>
2024-09-18 19:09:15 +0530

    Added the unfollow user route

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,6 +1,6 @@
 import express from "express"
 import isAuthenticated from "../config/auth.js";
-import { bookmark, follow, getMyProfile, getOtherUsers, Login, logout, Register } from "../controllers/userController.js";
+import { bookmark, follow, getMyProfile, getOtherUsers, Login, logout, Register, unfollow } from "../controllers/userController.js";
 
 const router = express.Router();
 
@@ -11,5 +11,6 @@ router.route("/bookmark/:id").put(isAuthenticated, bookmark);
 router.route("/profile/:id").get(isAuthenticated, getMyProfile);
 router.route("/otheruser/:id").get(isAuthenticated, getOtherUsers);
 router.route("/follow/:id").post(isAuthenticated, follow);
+router.route("/unfollow/:id").post(isAuthenticated, unfollow);
 
 export default router
