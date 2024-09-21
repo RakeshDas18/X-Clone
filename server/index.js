d332b9f43516562eac110dacddaeeb1ae45b6bef
Rakesh Das <18.rakeshdas@gmail.com>
2024-09-21 19:01:36 +0530

    Use cors for origin of the client site url

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import databaseConnection from "./config/database.js";
 import cookieParser from "cookie-parser";
 import userRoute from "./routes/userRoute.js"
 import tweetRoute from "./routes/tweetRoute.js"
+import cors from "cors";
 
 
 dotenv.config({
@@ -15,10 +16,17 @@ const app = express();
 
 //Middlewares
 app.use(express.urlencoded({
-    extends:true
+    extended:true
 }))
 app.use(express.json());
 app.use(cookieParser());
+
+const corsOption = {
+    origin:"http://localhost:3000",
+    credentials:true
+}
+
+app.use(cors(corsOption))
 
 //routes
 app.use("/api/v1/user", userRoute)
