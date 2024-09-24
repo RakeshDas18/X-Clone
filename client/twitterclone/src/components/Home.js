582ff7ec62a139adbe30222a259876f07df17ae4
Rakesh Das <18.rakeshdas@gmail.com>
2024-09-24 21:33:29 +0530

    Fixed some issues

--- client/twitterclone/src/components/Home.js
+++ client/twitterclone/src/components/Home.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import LeftSiderbar from './LeftSiderbar'
-import Feed from './Feed'
 import RightSidebar from './RightSidebar'
 import { Outlet } from 'react-router-dom'
 
