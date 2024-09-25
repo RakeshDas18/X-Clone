be5c529d55791984c08d7b198bf079e5e4ef4663
Rakesh Das <18.rakeshdas@gmail.com>
2024-09-25 21:35:31 +0530

    Added some hooks to get user and otherusers

--- client/twitterclone/src/components/Home.js
+++ client/twitterclone/src/components/Home.js
@@ -2,13 +2,19 @@ import React from 'react'
 import LeftSiderbar from './LeftSiderbar'
 import RightSidebar from './RightSidebar'
 import { Outlet } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import useOtherUsers from '../hooks/useOtherUsers.js'
 
 function Home() {
+  //hooks
+  const { user, otherUsers } = useSelector(store=>store.user)
+  useOtherUsers(user?._id)
+
   return (
     <div className='flex justify-between w-[90%] mx-auto'>
       <LeftSiderbar/>
       <Outlet/>
-      <RightSidebar/>
+      <RightSidebar otherUsers={otherUsers}/>
     </div>
   )
 }
