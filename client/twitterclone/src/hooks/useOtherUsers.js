a72ce0344503851e19adbfff5b878b19d103061e
Rakesh Das <18.rakeshdas@gmail.com>
2024-09-25 21:37:26 +0530

    Coded a custom hook to fetch other users

---
+++ client/twitterclone/src/hooks/useOtherUsers.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+import { USER_API_END_POINT } from "../utils/constant";
+import { useEffect } from "react";
+import {useDispatch} from "react-redux";
+import { getMyProfile, getOtherUser } from "../redux/userSlice";
+
+const useOtherUsers = (id) => {
+    const dispatch = useDispatch();
+    useEffect(()=>{
+        const fetchOtherUsers = async () => {
+            try {
+                const res = await axios.get(`${USER_API_END_POINT}/otheruser/${id}`,{
+                    withCredentials:true
+                });
+                console.log(res);
+                dispatch(getOtherUser(res.data.otherUsers));
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        fetchOtherUsers();
+    },[id, dispatch]);
+};
+export default useOtherUsers;
