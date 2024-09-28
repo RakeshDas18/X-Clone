0df9387499cb2a7f2bf4829790ec78225b48483c
Rakesh Das <18.rakeshdas@gmail.com>
2024-09-28 21:49:32 +0530

    Get the tweets from store and mapped them

--- client/twitterclone/src/components/Feed.js
+++ client/twitterclone/src/components/Feed.js
@@ -1,16 +1,20 @@
-import React from 'react'
-import CreatePost from './CreatePost'
-import Tweet from './Tweet'
+import React from "react";
+import CreatePost from "./CreatePost";
+import Tweet from "./Tweet";
+import { useSelector } from "react-redux";
 
 function Feed() {
+  const { tweets } = useSelector(store=>store.tweet);
   return (
-    <div className='w-[50%] border border-gray-200'>
+    <div className="w-[50%] border border-gray-200">
       <div>
-        <CreatePost/>
-        <Tweet/>
+        <CreatePost />
+        {tweets?.map((tweet) => {
+          <Tweet key={tweet?._id} tweet={tweet} />;
+        })}
       </div>
     </div>
-  )
+  );
 }
 
-export default Feed
+export default Feed;
