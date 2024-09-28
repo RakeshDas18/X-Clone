bdce102b04c06698dfb533681741f2f3ee82cd4c
Rakesh Das <18.rakeshdas@gmail.com>
2024-09-28 21:50:15 +0530

    Added a custom hook to get all the tweets

--- client/twitterclone/src/components/Home.js
+++ client/twitterclone/src/components/Home.js
@@ -4,11 +4,14 @@ import RightSidebar from './RightSidebar'
 import { Outlet } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 import useOtherUsers from '../hooks/useOtherUsers'
+import useGetMyTweets from '../hooks/useGetMyTweets'
 
 function Home() {
+  const { user, otherUsers } = useSelector(store=>store.user);
+  
   //hooks
-  const { user, otherUsers } = useSelector(store=>store.user)
-  useOtherUsers(user?._id)
+  useOtherUsers(user?._id);
+  useGetMyTweets(user?._id);
 
   return (
     <div className='flex justify-between w-[90%] mx-auto'>
