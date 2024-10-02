ea57096ef7011dce93c407074a0eb369c693c0be
Rakesh Das <18.rakeshdas@gmail.com>
2024-10-02 19:11:52 +0530

    Fixed some bugs

--- client/twitterclone/src/components/Feed.js
+++ client/twitterclone/src/components/Feed.js
@@ -1,20 +1,20 @@
-import React from "react";
-import CreatePost from "./CreatePost";
-import Tweet from "./Tweet";
+import React from 'react'
+import CreatePost from './CreatePost.js'
+import Tweet from './Tweet.js'
 import { useSelector } from "react-redux";
 
-function Feed() {
-  const { tweets } = useSelector(store=>store.tweet);
+const Feed = () => {
+  const {tweets} = useSelector(store=>store.tweet);
   return (
-    <div className="w-[50%] border border-gray-200">
+    <div className='w-[50%] border border-gray-200'>
       <div>
-        <CreatePost />
-        {tweets?.map((tweet) => {
-          <Tweet key={tweet?._id} tweet={tweet} />;
-        })}
+        <CreatePost/>
+        {
+          tweets?.map((tweet)=> <Tweet key={tweet?._id} tweet={tweet}/>)
+        }         
       </div>
     </div>
-  );
+  )
 }
 
-export default Feed;
+export default Feed
