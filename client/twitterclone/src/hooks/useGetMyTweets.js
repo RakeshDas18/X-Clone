772f59e981aab80ae1eba1a19e82fb545cbcd03b
Rakesh Das <18.rakeshdas@gmail.com>
2024-10-18 20:03:26 +0530

    Coded the For You and Following functions

--- client/twitterclone/src/hooks/useGetMyTweets.js
+++ client/twitterclone/src/hooks/useGetMyTweets.js
@@ -1,25 +1,35 @@
 import axios from "axios";
 import { TWEET_API_END_POINT } from "../utils/constant";
 import { useEffect } from "react";
-import {useDispatch, useSelector} from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { getAllTweets } from "../redux/tweetSlice";
 
 const useGetMyTweets = (id) => {
-    const dispatch = useDispatch();
-    const { refresh } = useSelector(store => store.tweet);
-    useEffect(()=>{
-        const fetchMyTweets = async () => {
-            try {
-                const res = await axios.get(`${TWEET_API_END_POINT}/alltweet/${id}`,{
-                    withCredentials:true
-                });
-                console.log(res);
-                dispatch(getAllTweets(res.data.tweets));
-            } catch (error) {
-                console.log(error);
-            }
-        }
-        fetchMyTweets();
-    },[id, refresh, dispatch]);
+  const dispatch = useDispatch();
+  const { refresh } = useSelector((store) => store.tweet);
+
+  const fetchMyTweets = async () => {
+    try {
+      const res = await axios.get(`${TWEET_API_END_POINT}/alltweet/${id}`, {
+        withCredentials: true,
+      });
+      console.log(res);
+      dispatch(getAllTweets(res.data.tweets));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const followingTweetHandler = async () => {
+    try {
+      const res = await axios.get(`${TWEET_API_END_POINT}/following/${id}`);
+      console.log(res);
+      dispatch(getAllTweets(res.data.message));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    fetchMyTweets();
+  }, [id, refresh, dispatch]);
 };
 export default useGetMyTweets;
