e087954d1bcc555fe03e3e4e66d879248edb6549
Rakesh Das <18.rakeshdas@gmail.com>
2024-10-25 19:38:30 +0530

    Added followingUpdate to update the state when follow and follow

--- client/twitterclone/src/redux/userSlice.js
+++ client/twitterclone/src/redux/userSlice.js
@@ -16,9 +16,18 @@ const userSlice = createSlice({
         },
         getMyProfile:(state, action) => {
             state.profile = action.payload;
+        },
+        followingUpdate:(state,action)=>{
+            if(state.user.following.includes(action.payload)){
+                state.user.following = state.user.following.filter((itemId)=>{
+                    return itemId !== action.payload;
+                })
+            }else{
+                state.user.following.push(action.payload);
+            }
         }
     }
 })
 
-export const {getUser, getOtherUser, getMyProfile} = userSlice.actions;
+export const {getUser, getOtherUser, getMyProfile, followingUpdate} = userSlice.actions;
 export  default userSlice.reducer;
