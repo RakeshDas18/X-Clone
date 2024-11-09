b762457ee3b49ca5192dcdac077364100a88d039
Rakesh Das <18.rakeshdas@gmail.com>
2024-11-09 12:31:52 +0530

    Correct the message when logout

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -74,7 +74,7 @@ export const Login = async (req, res) => {
 }
 export const logout = (req, res) => {
     return res.cookie("token", "", { expiresIn: new Date(Date.now()) }).json({
-        message: "user logged out successfully.",
+        message: "User logged out successfully.",
         success: true
     })
 }
